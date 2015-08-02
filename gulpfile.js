a8e58a827b62cae48d09737c970e4ad0409c3f84
Piermaria Cosina <piermaria@belafonte.co>
2015-08-02 12:15:24 +0200

    app generated with yo

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  plumber = require('gulp-plumber'),
+  livereload = require('gulp-livereload'),
+  stylus = require('gulp-stylus');
+
+gulp.task('stylus', function () {
+  gulp.src('./public/css/*.styl')
+    .pipe(plumber())
+    .pipe(stylus())
+    .pipe(gulp.dest('./public/css'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./public/css/*.styl', ['stylus']);
+});
+
+gulp.task('develop', function () {
+  livereload.listen();
+  nodemon({
+    script: 'bin/www',
+    ext: 'js jade coffee',
+  }).on('restart', function () {
+    setTimeout(function () {
+      livereload.changed(__dirname);
+    }, 500);
+  });
+});
+
+gulp.task('default', [
+  'stylus',
+  'develop',
+  'watch'
+]);
