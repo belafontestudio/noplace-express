860ece786adbeb2312386a5799587af17c7d123a
Piermaria Cosina <piermaria.cosina@gmail.com>
2016-06-20 17:54:11 +0200

    new instagram key

--- public/js/main.js
+++ public/js/main.js
@@ -22,19 +22,28 @@ function toggleSomething() {
 
 var timer = setInterval(toggleSomething, 2000);
 // 1000 = Initial timer when the page is first loaded
-
-
-$( "#mailchimp" ).submit(function( event ) {
-  event.preventDefault();
-  var email = $("input.email").val();
-  $.ajax({
-    method: "POST",
-    url: "/list/"+email+"/subscribe",
-  })
-  .done(function( res ) {
-    console.log(res);
-  })
-  .fail(function( jqXHR, textStatus ) {
-    alert( "Request failed: " + textStatus );
-  });
+$( document ).ready(function() {
+    console.log( "ready!" );
+    mailchimp();
 });
+
+function mailchimp(){
+  $( "#mailchimp" ).submit(function( event ) {
+    event.preventDefault();
+    var email = $("input.email").val();
+    $.ajax({
+      method: "POST",
+      url: "/list/"+email+"/subscribe",
+    })
+    .done(function( res ) {
+      console.log(res);
+      $("#success").show();
+      $("#error").hide();
+    })
+    .fail(function( jqXHR, textStatus ) {
+      $("#error").show();
+      $("#success").hide();
+
+    });
+  });
+}
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ Instagram = require('instagram-node-lib');
 
 Instagram.set('client_id', '9e636d14946745eab4a5e91e2d53054c');
 Instagram.set('client_secret', '786e44b851954931a1faed247ba36444');
-Instagram.set('access_token', '294322778.5b9e1e6.805cb55b73d847e2be4e6ce899221464');
+Instagram.set('access_token', '294322778.1677ed0.f4dad895c4014f9c897a3a137531cd16');
+Instagram.set('callback_url', 'http://noplacemagazine.com');
+Instagram.set('redirect_uri', 'http://noplacemagazine.com');
 /* GET home page. */
 
 router.get('/', function(req, res) {
@@ -15,6 +17,7 @@ router.get('/', function(req, res) {
       // data is a javascript object/array/null matching that shipped Instagram
       // when available (mostly /recent), pagination is a javascript object with the pagination information
       //console.log(data, pagination);
+      console.log(data)
       res.locals.instagrams = data;
       res.render('index');
     },
@@ -27,7 +30,6 @@ router.get('/', function(req, res) {
     }});
 
 });
-
 
 
 module.exports = router;
